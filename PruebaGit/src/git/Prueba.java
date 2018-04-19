be12c46c5da239024219b2dfd8b5db342df7af1a
IEUser <IEUser@192.168.190.134>
2018-04-19 00:17:29 -0700

    Primera Versión

--- PruebaGit/src/git/Prueba.java
+++ PruebaGit/src/git/Prueba.java
@@ -5,6 +5,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Hello git 2!");
+		System.out.println("Hello git 3!");
 
 	}
 
